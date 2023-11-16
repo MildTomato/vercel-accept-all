a440ae313bde884f1dfdb3ac348fdcc138f4b91e
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-11-16 14:46:59 +0800

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -29,12 +29,18 @@ function IndexPopup() {
   }
 
   return (
-    <div style={{ width: "720px" }}>
-      <h1>Vercel Deployment SupaAuthorizer</h1>
-      <button onClick={() => handleClick("openTabs")}>Get links</button>
-      <button onClick={() => approveAllVercelTabs()}>
-        Approve all Vercel tabs
-      </button>
+    <div style={{ width: "720px", fontFamily: "monospace", padding: "12px" }}>
+      <h1 style={{ margin: "0" }}>vercel-accept-all</h1>
+      <p>Approve all vercel deployments in 2 clicks</p>
+      <p>Only works when viewing a Pull Request on GitHub</p>
+      <div style={{ display: "flex", gap: "8px" }}>
+        <button onClick={() => handleClick("openTabs")}>
+          Open all authorization tabs
+        </button>
+        <button onClick={() => approveAllVercelTabs()}>
+          Approve all Vercel tabs
+        </button>
+      </div>
     </div>
   )
 }
